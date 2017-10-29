a28e552da0657c885332c05767801e0fd47c5c17
Teepop Chandavongs <zlanfa@gmail.com>
2017-10-29 10:09:18 +0700

    Production

---
+++ production/config/config/database.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+
+    'fetch' => PDO::FETCH_CLASS,
+
+    'default' => env('DB_CONNECTION', 'mysql'),
+
+    'connections' => [
+
+        'mysql' => [
+            'driver'    => 'mysql',
+            'host'      => env('DB_HOST', '13.228.160.237'),
+            'database'  => env('DB_DATABASE', 'my_database_prod'),
+            'username'  => env('DB_USERNAME', 'my_user'),
+            'password'  => env('DB_PASSWORD', 'my_password'),
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+            'strict'    => false,
+            'engine'    => null,
+        ],
+    ],
+
+    'migrations' => 'migrations',
+
+    'redis' => [
+
+        'cluster' => false,
+
+        'default' => [
+            'host'     => env('REDIS_HOST', 'localhost'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port'     => env('REDIS_PORT', 6379),
+            'database' => 0,
+        ],
+
+    ],
+
+];
